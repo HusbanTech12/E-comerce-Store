076ba6a27567fb16d6598f481306eddb7dcbef7b
HusbanTech12 <husbantech08@gmail.com>
2024-12-09 17:41:25 +0500

    add header cards footer

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+export default function Home() {
+  return (
+
+    <>
+      <div className="w-[1440px] h-[6197px] ">
+        <Header />
+        <Cards />
+
+
+
+      </div>
+
+
+    
+    </>
+
+
+
+  )    
+  
+
+}
